5f326c51246a7f028988bf5fbc5f24ac557ae184
gin.arango <gin.arango@69201f73-9e3d-0410-8f0c-e7542da299f8>
2008-02-15 08:10:57 +0000

    Insert in DB

---
+++ src/replics/impl/data/kernel/test.java
@@ -0,0 +1,18 @@
+package replics.impl.data.kernel;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String s = "g";
+		Byte b = new Byte(s);
+		 
+		System.out.println(s);
+		System.out.println(b);
+
+	}
+
+}
