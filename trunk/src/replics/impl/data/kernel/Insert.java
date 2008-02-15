28c4beaa7b8bdcca450efd8772bf28505996934f
gin.arango <gin.arango@69201f73-9e3d-0410-8f0c-e7542da299f8>
2008-02-15 08:10:57 +0000

    Insert in DB

--- trunk/src/replics/impl/data/kernel/Insert.java
+++ trunk/src/replics/impl/data/kernel/Insert.java
@@ -1,18 +1,41 @@
 package replics.impl.data.kernel;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+
 
 public class Insert {
 
-	static Connection con = null;
-	 
-	 public void Insert1(Byte SQLData[]) throws SQLException{
+	 public static void saveRecord(Connection con, String idGroup, Integer IDU, String hash, File xml, File photo, File fingerPrint, float height) throws SQLException, FileNotFoundException{
 		
+		  	String sql = "INSERT INTO test1 (groupId, IDU, hash, xml, photo, fingerPrint, height) VALUES(?,?,?,?,?,?,?)";
+	        PreparedStatement pstmt = con.prepareStatement(sql);
+	        
+	        pstmt.setString(1,idGroup);
+	        pstmt.setInt(2,IDU);
+	        pstmt.setString(3,hash);
+	        
+	        FileInputStream is = new FileInputStream(xml);
+	        pstmt.setAsciiStream(4, is, xml.length());
+	        
+	        is = new FileInputStream(photo);
+	        pstmt.setBinaryStream(5, is, photo.length());
+	        
+	        is = new FileInputStream(fingerPrint);
+	        pstmt.setBinaryStream(6, is, fingerPrint.length());
+	        
+	        pstmt.setFloat(7,height);
+	        
+		  	pstmt.executeUpdate();
 		  	
-		 	Statement stmt = con.createStatement();
-		  	stmt.executeUpdate("INSERT INTO olpc VALUES " + SQLData[0] );
+		  	System.out.println("Data successfully added to the DB");
 		  	
 	 }
+	 
+	 
 }
--- trunk/src/replics/impl/data/kernel/JDBC.java
+++ trunk/src/replics/impl/data/kernel/JDBC.java
@@ -1,29 +1,55 @@
 package replics.impl.data.kernel;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import replics.impl.data.MySqlDataProvider;
+
 public class JDBC {
 	
   static Connection con = null;
 
-  public static void main(String args[]) throws SQLException {
+  public static void main(String args[]) throws SQLException, IOException {
 	  
 	Connect();
-	CreateDB();
+//	CreateDB();
+	
+	String groupID = "groupID";
+	Integer IDU = 24474520;
+	String hash = "hash";
+	File xml = new File("data/fileXML.xml");
+	xml.createNewFile();
+	File photo = new File("data/photo.jpg");
+	photo.createNewFile();
+	File fingerPrint = new File("data/fingerPrint.fp");
+	fingerPrint.createNewFile();
+	float height = (float) 1.65 ;
+	
+	Insert.saveRecord(con, groupID, IDU, hash, xml, photo, fingerPrint, height);
+	
 	Disconnect();
 	
+	MySqlDataProvider mdp = new MySqlDataProvider();
+	
+	mdp.initialize();
+	
+	
+	mdp.saveRecord(null);
     }
   
 
     private static void Connect() {
 	try {
 	      Class.forName("com.mysql.jdbc.Driver").newInstance();
-	      String url ="jdbc:mysql://localhost:3306/special_tacou";
-	      con = DriverManager.getConnection(url,"tacou","tagisy6");
+	      String url ="jdbc:mysql://localhost:3306/gin";
+	      con = DriverManager.getConnection(url,"root",null);
 
 	      if(!con.isClosed())
 	        System.out.println("Successfully connected to MySQL server using TCP/IP...");
@@ -49,12 +75,13 @@ public class JDBC {
     	Statement stmt = con.createStatement();
     	
     	stmt.executeUpdate( "CREATE TABLE test1 ("  +
-    	         "recordId    	INT    		NOT NULL, "    +
+    	         "recordId    	INTEGER    		NOT NULL AUTO_INCREMENT, "    +
     	         "groupId    	VARCHAR(100)    NOT NULL, "    +
+    	         "IDU    		INTEGER			NOT NULL, "    +
     	         "hash    		VARCHAR(100)    NOT NULL, "    +
     	         "xml   		TEXT 			NOT NULL, "    +
-    	         "photo        	BINARY 			NOT NULL, "    +
-    	         "fingerPrint	BINARY 			NOT NULL, "    +
+    	         "photo        	LONGBLOB		NOT NULL, "    +
+    	         "fingerPrint	LONGBLOB		NOT NULL, "    +
     	         "height		FLOAT 			NOT NULL, "    +
     	         "PRIMARY KEY( recordID, groupId )"                  +
     	                                            ")" );
@@ -62,5 +89,3 @@ public class JDBC {
     	System.out.println("Table successfully created...");
       }
 }
-
-//system....exec(url); pour executer un program
